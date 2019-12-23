3c694d38522f9e55389f5e446f6215101f9105d0
ropeshen888 <58461469+ropeshen888@users.noreply.github.com>
2019-12-23 23:25:11 +0800

    test  comtainer

--- javascript/test.js
+++ javascript/test.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
     $("#container").html(''+
         '<h1>' +
         '    Hello world!!!' +
-        '<input id="play-btn" type="button" value="play">' +
+        '<input id="play-bt" type="button" value="play">' +
         '</h1>');
-});
-
-$("#play-btn").click(function() {
-    $("#container").html(''+
-        '<h1>' +
-        '     Hello world!' +
-        '</h1>');
+    
+     $("#play-bt").click(function() {
+         $("#container").html(''+
+             '<h1>' +
+             '     Hello world???' +
+             '</h1>');
+     });
 });
