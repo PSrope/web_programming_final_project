629c6a807bff418f222826114f80c209bacb0ef6
ropeshen888 <58461469+ropeshen888@users.noreply.github.com>
2019-12-23 22:41:04 +0800

    Update test.js

--- javascript/test.js
+++ javascript/test.js
@@ -2,12 +2,13 @@ $(document).ready(function() {
     $("#container").html(''+
         '<h1>' +
         '    Hello world' +
+        '<input id="play-btn" type="button" value="play">' +
         '</h1>');
 });
 
 $("#play-btn").click(function() {
     $("#container").html(''+
         '<h1>' +
-        '     Hello world' +
+        '     Hello world!' +
         '</h1>');
 });
