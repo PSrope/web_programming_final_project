ca7c08e6491bcfaa74319dbd807ba1af75ca22f9
ropeshen888 <58461469+ropeshen888@users.noreply.github.com>
2019-12-23 22:54:14 +0800

    css file test

--- javascript/test.js
+++ javascript/test.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $("#container").html(''+
         '<h1>' +
         '    Hello world!!!' +
+        '<input id="play-btn" type="button" value="play">' +
         '</h1>');
 });
 
