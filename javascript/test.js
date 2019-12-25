7642676c2c27784262a5329f268daf2e707d2b9c
ropeshen888 <58461469+ropeshen888@users.noreply.github.com>
2019-12-25 09:11:29 +0800

    put code1 in container
    就如題

--- javascript/test.js
+++ javascript/test.js
@@ -1,9 +1,16 @@
+function myClick()
+{
+	alert("123");
+}
+
 $(document).ready(function() {
     $("#container").html(''+
-        '<h1>' +
-        '    Hello world!!!' +
-        '<input id="play-bt" type="button" value="play">' +
-        '</h1>');
+    '<h1 align="center"><font face="fantasy">BaseBall Game Simulator</font></h1>' +
+    '<img style="display:block; margin:auto;" src="ball.jpeg" border="2"></img>' +
+    '<p align ="center">您將要在五組預設隊伍中選擇兩個隊伍:teamRED 和 teamBLUE 來進行一場對抗賽</p>' +
+    '<div style="">  ' +
+    '<center><input type="submit" onClick="myClick()"  value="Game Start" class="submit-btn" ></input></center>' +
+    '   </div>');
     
      $("#play-bt").click(function() {
          $("#container").html(''+
