ec1e87e6e92580cfe0d1f4454d8a96c9ad685adc
ropeshen888 <58461469+ropeshen888@users.noreply.github.com>
2019-12-25 10:04:31 +0800

    contain code1 code2

--- javascript/test.js
+++ javascript/test.js
@@ -8,14 +8,31 @@ $(document).ready(function() {
     '<h1 align="center"><font face="fantasy">BaseBall Game Simulator</font></h1>' +
     '<img style="display:block; margin:auto;" src="ball.jpeg" border="2"></img>' +
     '<p align ="center">您將要在五組預設隊伍中選擇兩個隊伍:teamRED 和 teamBLUE 來進行一場對抗賽</p>' +
-    '<div style="">  ' +
-    '<center><input type="submit" onClick="myClick()"  value="Game Start" class="submit-btn" ></input></center>' +
-    '   </div>');
+    '<div>  ' +
+    '<center><input id="submit-btn" type="submit" onClick="myClick()"  value="Game Start" class="submit-btn" ></input></center>' +
+    '</div>');
     
-     $("#play-bt").click(function() {
+     $("#submit-btn").click(function() {
          $("#container").html(''+
-             '<h1>' +
-             '     Hello world???' +
-             '</h1>');
+         '<h1 align="center"><font face="fantasy">Please Select  Team:</font></h1>' +
+         '<div>' +
+         '  <img  style="border:2px green solid; display:block; margin:auto; " src="https://dpsvdv74uwwos.cloudfront.net/statics/img/ogimage/red-team-vs-blue-team.png">' +
+         '<div style = "position:absolute;  background-color:#C7FF91; left:400px;  top:300px ;   width:300px;height:200px;border:3px #CC0000  solid;">' +
+         '  1' +
+         '</div> ' +
+         '   <div style = "position:absolute; background-color:#C7FF91; right:400px;  top:350px;    width:300px;height:200px;border:3px #CC0000  solid;">' +
+         '     2d' +
+         '  </div>' +
+         '  <div style = "position:absolute; background-color:#C7FF91; left:400px;  top:550px;    width:300px;height:40px;border:3px #CC0000  solid;">' +
+         '     3' +
+         '  </div>' +
+         '  <div style = "position:absolute; background-color:#C7FF91; right:400px;  top:250px;    width:300px;height:40px;border:3px #CC0000  solid;">' +
+         '     4' +
+         '  </div>' +
+         '  </div>' +
+         '  <div style = "position:absolute; left:800px;  top:750px ;   width:300px;height:200px;">' +
+         '      <center><input type="submit" onClick="myClick()"  value="READY" class="submit-btn" ></input></center>' +
+         '  </div>'
+             );
      });
 });
